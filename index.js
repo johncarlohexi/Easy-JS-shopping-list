b83283565447d587afb964e6a18e7ca3df690db9
johncarlohexi <hexkundiman@gmail.com>
2022-06-16 02:36:02 +0900

    Added space to items

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function addToList() {
     if(getValue === "") {
         return false
     }
-    let addToCart = cart.push(getValue)
-    console.log(addToCart)
+    let getValueSpace = getValue + "  ";
+    let addToCart = cart.push(getValueSpace)
     get.textContent = addToCart;
     show.textContent = cart.toString();
 }
